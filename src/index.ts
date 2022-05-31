7647c402e02d6886317058404ea57f3c8cbe7e4c
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-31 14:05:42 -0700

    lesson on the any type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@
 let sales = 123_456_789;
 let course: string = 'TypeScript';
 let isPublshed: boolean = true;
+
+// assumed to be the "any" type
+// defeats purpose of TypeScript type checking.  Avoid if possible
+let level;
+level = 1;
+level = "a";
+
+
+function render(document){
+  console.log(document);
+  
+}
