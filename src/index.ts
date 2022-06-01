875e34e185bb113a74500c48402302ded573a5e2
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-01 08:03:55 -0700

    example functions

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,12 @@ const enum Size { Small, Medium, Large }
 
 let mySize: Size = Size.Small;
 console.log(mySize);
+
+// Funtions.  question mark makes argument optional 
+function calculateTax(income: number, taxYear = 2022): number{
+  if(taxYear < 2022)
+    return income * 1.2;
+  return income * 1.3  
+}
+
+calculateTax(10_000, 2022)
